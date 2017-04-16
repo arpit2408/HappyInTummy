c7bf8d6cbb477ba410c9db768a0e345fbd71af9d
Rini Shaji <rinishaji@gmail.com>
2017-04-16 18:17:06 -0500

    Rini- Manage Order New Files

---
+++ HappyInTummy/src/happytummy/servlet/CheckUserExists.java
@@ -0,0 +1,75 @@
+package happytummy.servlet;
+
+import happytummy.connect.ConnectionUtils;
+import happytummy.utils.DBUtils;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * Servlet implementation class CheckUserExists
+ */
+public class CheckUserExists extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CheckUserExists() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Entered doPost of CheckUSerExists "+ request.getParameter("email_id")+" "+request.getParameter("dob")); //test
+		Connection conn;
+		try {
+			boolean userFound=false;
+			conn = ConnectionUtils.getConnection();
+			conn.setAutoCommit(false);
+			String errorString = null;
+			String emailID=request.getParameter("email_id");
+			String dob=request.getParameter("dob");			
+			try{	   
+				DateFormat sourceFormat=new SimpleDateFormat("yyyy-MM-dd");
+				DateFormat destFormat=new SimpleDateFormat("dd-MMM-yyyy");
+				Date birthDate = sourceFormat.parse(dob);
+				dob=destFormat.format(birthDate);
+				userFound =DBUtils.userExists(conn, emailID, dob);
+			} catch (Exception e) {
+	            e.printStackTrace();
+	            errorString = e.getMessage();
+	        }
+			response.setContentType("application/json");
+	        response.setHeader("Cache-control", "no-cache, no-store");
+	        response.setCharacterEncoding("utf-8");
+	        PrintWriter out = response.getWriter();
+	        out.print(userFound);
+		}catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
